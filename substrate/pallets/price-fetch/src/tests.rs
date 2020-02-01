da83a932fca051a387c7a4536ab7ee7d87fa844d
Jimmy Chu <jimmychu0807@gmail.com>
2020-02-01 16:45:35 +0800

    wip: adding test code

--- substrate/pallets/price-fetch/src/tests.rs
+++ substrate/pallets/price-fetch/src/tests.rs
@@ -8,11 +8,27 @@
 //  3. with multiple record_price of same sym inserted. On next cycle, the average of the price is calculated
 //  4. can fetch for BTC, parse the JSON blob and get a price > 0 out
 
+use rstd::{prelude::*};
+use codec::{Encode, Decode};
+use crate::*;
 use crate::mock::*;
+use support::{assert_ok};
 
 #[test]
 fn it_works_for_default_value() {
   new_test_ext().execute_with(|| {
-    assert_eq!(1, 1);
+    let sym = b"CCC";
+    let remote_src = b"XXX";
+    let remote_url = b"http://test.com";
+
+    // Given that nothing in the vector
+    let vec = <TokenSrcPPMap<TestRuntime>>::get(sym.to_vec());
+    println!("The vec: {:?}", vec);
+
+
+
+    // We record a src price into the state machine
+    // assert_ok!(PriceFetchModule::record_price(Origin::signed(1), 1, (&sym, &remote_src,
+    //   &remote_url), 100));
   });
 }
